d5141cd2b3877782850b87de7c746f60f035546c
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-16 21:39:13 -0300

    :sparkles: add celebrate middleware & config celebrate in errorHandler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import { errorHandler } from "~middlewares/errorHandler";
+import { Joi, celebrate } from "celebrate";
 
 const app = express();
 
@@ -11,7 +12,11 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", celebrate({
+  query: {
+    name: Joi.string().required(),
+  }
+}), (req, res) => {
   throw new Error("test")
   res.json({ message: "hello world" });
 });
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,3 +1,4 @@
+import { isCelebrateError } from "celebrate";
 import type { Request, Response, NextFunction } from "express";
 import { AppException } from "~exceptions/AppException";
 
@@ -11,7 +12,25 @@ export const errorHandler = (
     return response.status(error.statusCode).json({ message: error.message });
   }
 
+  if (isCelebrateError(error)) {
+    const types = ["body", "query", "headers"];
+
+    const errors = types.reduce((acc, type) => {
+      const celebrateError = error.details.get(type)?.details[0].message;
+
+      if (!celebrateError) return acc;
+
+      const formattedErrorMessage = celebrateError.replace(/"/g, "");
+
+      return [...acc, formattedErrorMessage];
+    }, [] as string[]);
+
+    return response.status(400).json({
+      message: errors[0], // return only the first error
+    });
+  }
+
   return response.status(500).json({
-    message: `Erro interno no servidor - ${error.message}`,
+    message: `Internal server error: ${error.message}`,
   });
 };
