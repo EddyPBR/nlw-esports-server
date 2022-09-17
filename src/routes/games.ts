d8f9974497a86d80d300729d11037f92a7c5c7d9
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-17 00:12:38 -0300

    :sparkles: implements new route

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import { celebrate, Joi } from "celebrate";
+
 import { PrismaGamesRepository } from "~repositories/prisma/PrismaGamesRepository";
 import { GetHighlightedGamesUseCase } from "~useCases/GetHighlightedGamesUseCase";
+import { GetAdsByGameUseCase } from "~useCases/GetAdsByGameUseCase";
+import { PrismaAdsRepository } from "~repositories/prisma/PrismaAdsRepository";
 
 const router = Router();
 
@@ -15,5 +19,27 @@ router.get("/", async (_request, response) => {
 
   return response.status(200).json(highlightedGames);
 });
+
+router.get(
+  "/:gameId/ads",
+  celebrate({
+    params: {
+      gameId: Joi.string().not().empty().required(),
+    },
+  }),
+  async (request, response) => {
+    const gameId = request.params.id;
+
+    const prismaAdsRepository = new PrismaAdsRepository();
+
+    const getAdsByGameUseCase = new GetAdsByGameUseCase(
+      prismaAdsRepository
+    );
+
+    const gameAds = await getAdsByGameUseCase.handle(gameId);
+
+    return response.status(200).json(gameAds);
+  }
+);
 
 export { router };
