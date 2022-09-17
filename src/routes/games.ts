b99a31226c14a289c7a7842e2901815f846df616
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-17 02:21:13 -0300

    :sparkles: implements new route to create ads

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -5,6 +5,9 @@ import { PrismaGamesRepository } from "~repositories/prisma/PrismaGamesRepositor
 import { GetHighlightedGamesUseCase } from "~useCases/GetHighlightedGamesUseCase";
 import { GetAdsByGameUseCase } from "~useCases/GetAdsByGameUseCase";
 import { PrismaAdsRepository } from "~repositories/prisma/PrismaAdsRepository";
+import { convertStringToMinute } from "~utils/convertHourStringToMinutes";
+import { CreateAdUseCase } from "~useCases/CreateAdUseCase";
+import { convertMinutesToHourString } from "~utils/convertMinutesToHourString";
 
 const router = Router();
 
@@ -28,7 +31,7 @@ router.get(
     },
   }),
   async (request, response) => {
-    const gameId = request.params.id;
+    const gameId = request.params.gameId;
 
     const prismaAdsRepository = new PrismaAdsRepository();
 
@@ -42,6 +45,60 @@ router.get(
     }));
 
     return response.status(200).json(formattedGameAds);
+  }
+);
+
+router.post(
+  "/:gameId/ads",
+  celebrate({
+    params: {
+      gameId: Joi.string().not().empty().required(),
+    },
+    body: {
+      name: Joi.string().required(),
+      yearsPlaying: Joi.number().required(),
+      discord: Joi.string().required(),
+      weekDays: Joi.array().items(Joi.number()).required(),
+      hourStart: Joi.string().not().empty().min(5).max(5).required(),
+      hourEnd: Joi.string().not().empty().min(5).max(5).required(),
+      useVoiceChannel: Joi.boolean().required(),
+    },
+  }),
+  async (request, response) => {
+    const gameId = request.params.gameId;
+    const body = request.body as {
+      name: string;
+      yearsPlaying: number;
+      discord: string;
+      weekDays: number[];
+      hourStart: string;
+      hourEnd: string;
+      useVoiceChannel: boolean;
+    };
+
+    const data = {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hourStart: convertStringToMinute(body.hourStart),
+      hourEnd: convertStringToMinute(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    };
+
+    const prismaAdsRepository = new PrismaAdsRepository();
+
+    const createAdUseCase = new CreateAdUseCase(prismaAdsRepository);
+
+    const ad = await createAdUseCase.handle(data);
+
+    response.status(202).json({
+      ...ad,
+      weekDays: ad.weekDays.split(","),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd),
+    });
   }
 );
 
